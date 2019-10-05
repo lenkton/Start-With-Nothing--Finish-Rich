4f055a9f33066a3fcd4c715f3c8f9ea3c15088c0
lenkton <lenktongm@gmail.com>
2019-10-05 04:02:55 +0300

    Basic Mechanics of RPG

---
+++ Start with nothing, finish rich/Assets/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public CanvasGroup CanvasGroup;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = FindObjectOfType<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        text.text = "Hey, you! Get out of here!";
+    }
+}
---
+++ Start with nothing, finish rich/Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = .01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit;
+            if (hit = Physics2D.Raycast(ray.origin, ray.direction * 100f)){
+                if (hit.transform.tag == "Character")
+                {
+                    hit.transform.GetComponent<Character>().Interact();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal")*speed;
+        transform.position = pos;
+    }
+}
