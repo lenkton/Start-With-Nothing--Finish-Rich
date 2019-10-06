30f14c1af78aa58f2fa9b908699f9391d59aaa4b
lenkton <lenktongm@gmail.com>
2019-10-07 00:13:20 +0300

    Bad Practice Scene Change

---
+++ Start with nothing, finish rich/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger:MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform shop;
+    [SerializeField]
+    private Transform CharacterLayer;
+    public void GoToShop()
+    {
+        Vector3 pos = player.position;
+        pos.z = shop.position.z + CharacterLayer.position.z;
+        player.position = pos;
+    }
+}
